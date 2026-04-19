f85bf0f7953888be02e8fc2cfa9b1e43f909f991
Yurii Oliinyk <oliinykyurii123@gmail.com>
2026-04-19 21:55:21 +0200

    refactor: minor import

--- composeApp/src/commonMain/kotlin/kupio/mobile/core/designsystem/KupioTheme.kt
+++ composeApp/src/commonMain/kotlin/kupio/mobile/core/designsystem/KupioTheme.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
@@ -79,7 +80,7 @@ fun KupioTheme(
         bodyLarge = TextStyle.Default.copy(fontFamily = FontFamily.SansSerif),
     )
 
-    androidx.compose.runtime.CompositionLocalProvider(
+    CompositionLocalProvider(
         LocalKupioSpacing provides KupioSpacing(),
     ) {
         MaterialTheme(

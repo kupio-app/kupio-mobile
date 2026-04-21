618d7899b6441ca333a9044c45f15ebf9e534151
Vadim Popovych <zyzel19@gmail.com>
2026-04-21 19:30:47 +0200

    refactor(auth): refactor session state resolution to extension function

--- composeApp/src/commonMain/kotlin/kupio/mobile/features/auth/domain/session/SessionStateResolver.kt
+++ composeApp/src/commonMain/kotlin/kupio/mobile/features/auth/domain/session/SessionStateResolver.kt
@@ -3,14 +3,5 @@ package kupio.mobile.features.auth.domain.session
 import kupio.mobile.features.auth.domain.model.AuthenticatedUser
 import kupio.mobile.features.auth.domain.model.SessionState
 
-class SessionStateResolver {
-    fun resolve(
-        user: AuthenticatedUser,
-    ): SessionState {
-        return if (user.needsUsername) {
-            SessionState.NeedsUsername(user)
-        } else {
-            SessionState.SignedIn(user)
-        }
-    }
-}
+fun AuthenticatedUser.toSessionState(): SessionState =
+    if (needsUsername) SessionState.NeedsUsername(this) else SessionState.SignedIn(this)

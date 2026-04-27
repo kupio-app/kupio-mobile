8c1d8ddf614f3bdb65ff2bdd2d80b1d4843397f4
Vadim Popovych <zyzel19@gmail.com>
2026-04-27 17:36:21 +0200

    feat(me): implement in repository listing's status changing

--- composeApp/src/commonMain/kotlin/kupio/mobile/features/me/data/repository/MeRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/kupio/mobile/features/me/data/repository/MeRepositoryImpl.kt
@@ -3,6 +3,7 @@ package kupio.mobile.features.me.data.repository
 import kupio.mobile.core.network.AuthenticatedApiClient
 import kupio.mobile.features.me.data.remote.MeApi
 import kupio.mobile.features.me.domain.model.OwnedListing
+import kupio.mobile.features.me.domain.model.OwnedListingStatus
 import kupio.mobile.features.me.domain.model.UserListingStats
 import kupio.mobile.features.me.domain.repository.MeRepository
 
@@ -16,4 +17,8 @@ class MeRepositoryImpl(
 
     override suspend fun getMyListings(): List<OwnedListing> =
         apiClient.request { meApi.getMyListings(it) }
+
+    override suspend fun updateListingStatus(listingId: String, status: OwnedListingStatus) {
+        apiClient.request { meApi.updateListingStatus(it, listingId, status) }
+    }
 }
--- composeApp/src/commonMain/kotlin/kupio/mobile/features/me/domain/repository/MeRepository.kt
+++ composeApp/src/commonMain/kotlin/kupio/mobile/features/me/domain/repository/MeRepository.kt
@@ -1,9 +1,11 @@
 package kupio.mobile.features.me.domain.repository
 
 import kupio.mobile.features.me.domain.model.OwnedListing
+import kupio.mobile.features.me.domain.model.OwnedListingStatus
 import kupio.mobile.features.me.domain.model.UserListingStats
 
 interface MeRepository {
     suspend fun getStats(): UserListingStats
     suspend fun getMyListings(): List<OwnedListing>
+    suspend fun updateListingStatus(listingId: String, status: OwnedListingStatus)
 }

b716ac042873d1e0518989c5d5f7bd3f34452b21
Vadim Popovych <zyzel19@gmail.com>
2026-05-01 10:42:23 +0200

    feat(notifications): initialize NotifierManager and schedule background sync in MainViewController(IOS)

--- composeApp/src/iosMain/kotlin/kupio/mobile/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/kupio/mobile/MainViewController.kt
@@ -1,13 +1,31 @@
 package kupio.mobile
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.mmk.kmpnotifier.notification.NotifierManager
+import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
 import kupio.mobile.app.App
 import kupio.mobile.core.di.initKoin
+import kupio.mobile.core.notifications.BackgroundSyncScheduler
+import kupio.mobile.core.notifications.PushNotificationManager
+import org.koin.mp.KoinPlatformTools
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController {
     initKoin()
-    return ComposeUIViewController {
-        App()
-    }
+
+    NotifierManager.initialize(
+        configuration = NotificationPlatformConfiguration.Ios(
+            showPushNotification = true,
+            askNotificationPermissionOnStart = false,
+        )
+    )
+
+    val koin = KoinPlatformTools.defaultContext().get()
+    val pushNotificationManager = koin.get<PushNotificationManager>()
+    pushNotificationManager.start()
+    NotifierManager.addListener(pushNotificationManager)
+
+    koin.get<BackgroundSyncScheduler>().schedule()
+
+    return ComposeUIViewController { App() }
 }

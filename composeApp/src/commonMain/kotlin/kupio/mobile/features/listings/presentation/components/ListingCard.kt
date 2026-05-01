c748def7fdcac6e2248f622a4a6fbec47b414486
Vadim Popovych <zyzel19@gmail.com>
2026-05-01 11:10:01 +0200

    chore: remove unused imports from ListingCard

--- composeApp/src/commonMain/kotlin/kupio/mobile/features/listings/presentation/components/ListingCard.kt
+++ composeApp/src/commonMain/kotlin/kupio/mobile/features/listings/presentation/components/ListingCard.kt
@@ -1,6 +1,5 @@
 ﻿package kupio.mobile.features.listings.presentation.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,7 +19,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp

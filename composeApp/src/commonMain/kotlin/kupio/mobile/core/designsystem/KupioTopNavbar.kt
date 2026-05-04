2cb83f3dc04bcb4f68753bf477fc3f763c408533
Vadim Popovych <zyzel19@gmail.com>
2026-05-04 22:04:49 +0200

    fix(chat): fix user avatar squashing in chat thread screen topbar

--- composeApp/src/commonMain/kotlin/kupio/mobile/core/designsystem/KupioTopNavbar.kt
+++ composeApp/src/commonMain/kotlin/kupio/mobile/core/designsystem/KupioTopNavbar.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -43,8 +42,8 @@ fun KupioTopNavbar(
         ) {
             if (leadingContent != null) {
                 Box(
-                    modifier = Modifier.size(40.dp),
-                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.widthIn(min = 40.dp),
+                    contentAlignment = Alignment.CenterStart,
                 ) {
                     leadingContent()
                 }
